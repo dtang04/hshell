16e5ab3560ea4453e32f880938b84d4042b79ac0
Dylan Tang <dtang04@uchicago.edu>
2026-02-27 16:43:46 -0600

    Support of ls, pwd, exit

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,30 @@
 module Main where
 
 import System.IO
+import System.Process
+
+data Command = Exit | Ls (Maybe String) | Pwd
 
 shell :: IO ()
 shell = do
     putStr "hshell> "
     input <- getLine
-    print (input)
-    shell
+    let cmds = words input
+    case cmds of
+        ["exit"]    -> execute Exit
+        ["ls"]      -> execute (Ls Nothing)
+        ["ls", dir] -> execute (Ls (Just dir))
+        ["pwd"]     -> execute (Pwd)
+        _           -> putStr "\nInvalid Command.\n" >> Main.shell
+
+execute :: Command -> IO ()
+execute Exit = putStrLn "Exiting hshell..."
+execute (Ls Nothing) = callCommand "ls" >> Main.shell
+execute (Ls (Just dir)) = callCommand ("ls " ++ dir) >> Main.shell
+execute Pwd = callCommand "pwd" >> Main.shell
+
 
 main :: IO ()
 main = do
     hSetBuffering stdout NoBuffering
-    shell
+    Main.shell
