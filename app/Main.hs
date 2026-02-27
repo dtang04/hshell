283f1066334a6799f109e457269fac726b78e0cb
Dylan Tang <dtang04@uchicago.edu>
2026-02-27 17:06:57 -0600

    Added cd, cd with argument, cd with ~

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,9 @@ module Main where
 
 import System.IO
 import System.Process
+import System.Directory
 
-data Command = Exit | Ls (Maybe String) | Pwd
+data Command = Exit | Ls (Maybe String) | Pwd | Cd (Maybe String)
 
 shell :: IO ()
 shell = do
@@ -15,6 +16,8 @@ shell = do
         ["ls"]      -> execute (Ls Nothing)
         ["ls", dir] -> execute (Ls (Just dir))
         ["pwd"]     -> execute (Pwd)
+        ["cd", dir] -> execute (Cd (Just dir))
+        ["cd"]      -> execute (Cd Nothing)
         _           -> putStr "\nInvalid Command.\n" >> Main.shell
 
 execute :: Command -> IO ()
@@ -22,7 +25,17 @@ execute Exit = putStrLn "Exiting hshell..."
 execute (Ls Nothing) = callCommand "ls" >> Main.shell
 execute (Ls (Just dir)) = callCommand ("ls " ++ dir) >> Main.shell
 execute Pwd = callCommand "pwd" >> Main.shell
-
+execute (Cd Nothing) = do
+    homeDir <- getHomeDirectory
+    setCurrentDirectory homeDir
+    Main.shell
+execute (Cd (Just ('~':'/':rest))) = do
+    homeDir <- getHomeDirectory
+    setCurrentDirectory (homeDir ++ "/" ++ rest)
+    Main.shell
+execute (Cd (Just dir))
+    | dir == "~" = execute (Cd Nothing)
+    | otherwise = setCurrentDirectory dir >> Main.shell
 
 main :: IO ()
 main = do
